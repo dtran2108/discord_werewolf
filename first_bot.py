37447cd984e135075d5e056912629978cd43f1e6
Trân Dương <dtran2108@gmail.com>
2019-06-04 21:26:38 +0700

    add debug line

--- first_bot.py
+++ first_bot.py
@@ -103,7 +103,8 @@ async def x(context):
             'lose': 'Tao ra ' + bot_choice + ', hên thôi nhường mày 1 lần.'
         }
         await client.say('Chị ra gì?')
-        response = client.wait_for_message(author=context.author)
+        response = client.wait_for_message(author=context.message.author)
+        print(context.message.author)
         print(response, type(response))
         # print(help(response))
         if response == 'kéo':
